ab0988386a059d4da82a43eb1292b9719c6316f9
jbyun <binifia@gmail.com>
2022-04-04 20:06:19 -0700

    crontab commit

---
+++ cronTest/mygarden.sh
@@ -0,0 +1,9 @@
+#mygarden.sh - github gardening
+#!/bin/sh
+
+#move to git repository
+cd /home/jbyun/eclipse-workspace/LinuxTest/cronTest
+git add .
+git commit -m "crontab commit"
+git push
+exit 0
